a5e87a555b80af9f608c88e4d614b2a70cea1808
张明辉 <361186482@qq.com>
2019-05-15 12:35:21 +0800

    主入口

---
+++ src/com/zzzmh/Application.java
@@ -0,0 +1,34 @@
+package com.zzzmh;
+
+/**
+ * 主方法入口
+ * 实现Nginx日志扫描分析并存数据库
+ *
+ * @anthor zmh
+ * @date 2019-5-15 12:29:37
+ */
+public class Application {
+
+    /**
+     * 扫描日志文件存数据库
+     *
+     * @param startDate 起始日期 例如 20190101
+     * @param endDate   结束日期 例如 20190430
+     */
+    public void ScannerAccessLog(String startDate,String endDate){
+
+    }
+
+    /**
+     * 扫描日志文件存数据库
+     *
+     * @param date 指定单个日期 例如 20190101
+     */
+    public void ScannerAccessLog(String date){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
